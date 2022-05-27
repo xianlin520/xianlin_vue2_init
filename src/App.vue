019f8c425245907d6edab09a8b917a2fa3e32437
XianLin <wanganning5201314@163.com>
2022-05-27 16:53:48 +0800

    提交Vue2项目工程文件v1.0

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,27 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+    <!-- 组件占位符 -->
     <router-view/>
   </div>
 </template>
 
 <style lang="less">
+body {
+  // 设置最小宽度, 以免出现布局混乱
+  min-width: 1800px;
+  margin: 0;
+  padding: 0;
+  border: 0;
+}
 #app {
+  // 设置字体
   font-family: Avenir, Helvetica, Arial, sans-serif;
+  // 让字体更平滑, 抗锯齿
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  // 内容居中
   text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  // 顶部不留白
+  margin-top: 0;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,34 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import HelloWorld from '@/components/HelloWorld'
+// 注册路由插件
 Vue.use(VueRouter)
 
 const routes = [
+  // 路由重定向
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HelloWorld
   },
+  // 只有用户访问到, 才会被加载渲染(惰性加载)
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/error',
+    name: 'error',
+    component: () => import('@/components/Error')
+  },
 ]
 
 const router = new VueRouter({
+  // 此模式下, 不会有 # 在 URL 中
   mode: 'history',
   base: process.env.BASE_URL,
   routes
 })
-
+// 路由前置守卫
+router.beforeEach((to, from, next) => {
+  // 通过
+  next()
+})
+// 将路由对象暴露出去
 export default router
